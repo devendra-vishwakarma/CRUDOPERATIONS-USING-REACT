4e66dd71162dd0400b38f0646e282c24ba71c207
Devendra <devendravishvakarma128@gmail.com>
2024-03-26 16:00:40 +0530

    new api create cmd

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,3 +1,4 @@
+// The command json-server --watch db.json is used to run a local JSON server. Here's what each part of the command does:
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
